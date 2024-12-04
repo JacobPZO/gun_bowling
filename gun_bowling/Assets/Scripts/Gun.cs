b1f28990ba73e9e911213d98b49470ae4d9766be
JacobPZO <156956701+JacobPZO@users.noreply.github.com>
2024-12-04 10:44:54 -0700

    gun scripting WIP
    slowly scripting
    
    shoutout youtube.com/watch?v=cl3E7_f74MA

---
+++ gun_bowling/Assets/Scripts/Gun.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    private bool AddBulletSpread = true;
+    [SerializeField]
+    private Vector3 BulletSpreadVariance = new Vector3(0.02f, 0.02f, 0.02f);
+    [SerializeField]
+    private ParticleSystem ShootingSystem;
+    [SerializeField]
+    private Transform BulletSpawnPoint;
+    [SerializeField]
+    private ParticleSystem ImpactParticleSystem;
+    [SerializeField]
+    private TrailRenderer BulletTrail;
+    [SerializeField]
+    private float ShootDelay = 0.5f;
+    [SerializeField]
+    private LayerMask Mask;
+
+    private Animator Animator;
+    private float LastShootTime;
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public void Shoot()
+    {
+        if (LastShootTime + ShootDelay < Time.time)
+        {
+            Animator.SetBool("IsShooting", true);
+            ShootingSystem.Play();
+            Vector3 direction = GetDirection();
+
+            if (Physics.Raycast(BulletSpawnPoint.position, direction, out RaycastHit hit, float.MaxValue, Mask))
+            {
+                TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+
+                StartCoroutine(SpawnTrail(trail, hit));
+
+                LastShootTime = Time.time;
+            }
+        }
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector3 direction = transform.forward;
+
+        if (AddBulletSpread)
+        {
+            direction += new Vector3(
+                Random.Range(-BulletSpreadVariance.x, BulletSpreadVariance.x),
+                Random.Range(-BulletSpreadVariance.y, BulletSpreadVariance.y),
+                Random.Range(-BulletSpreadVariance.z, BulletSpreadVariance.z)
+            );
+
+            direction.Normalize();
+        }
+
+        return direction;
+    }
+
+    
+}
